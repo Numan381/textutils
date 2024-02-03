3cc41e6bca07780eae97818fbeba48d4e93162ca
Numan381 <157959770+Numan381@users.noreply.github.com>
2024-02-03 15:38:47 +0500

    Update App.js
    Signed-off-by: Numan381 <157959770+Numan381@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,50 +1,91 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import About from './components/About';
-import Alert from './components/Alert';
-import React, { useState } from 'react';
-
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import React, { useState } from "react";
+import {
+  Outlet,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
-  const [alert, setAlert]=useState(null);
-  const showAlert =(message, type)=>{
-     setAlert({
-      msg:message,
-      type:type
-     })
-     setTimeout(() => {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
       setAlert(null);
-     }, 2000);
-  }    
-  const toggleMode =()=>{
-    if(mode=== 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor ='#042743';
-      showAlert("Dark mode has been enabled", "success")
-      document.title = 'TextUtils - Dark Mode';
-    }
-    else{
-      setMode ('light');
-      document.body.style.backgroundColor ='white';
-      showAlert("Light mode has been enabled", "success")
-      document.title = 'TextUtils - Light Mode'
-    }
+    }, 2000);
+  };
+  const removeBodyClasses =()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
   }
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    console.log(cls)
+    document.body.classList.add('bg-'+cls)
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+      } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      }
+  };
+  const router = createBrowserRouter([{},
+    {
+      path: "/",
+      element: (
+        <div>
+          <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+          <Alert alert={alert} />
+          <div>
+            <Outlet />
+          </div>
+        </div>
+      ),
+      children: [
+        {
+          path: "/",
+          index: true,
+          element: (
+            <TextForm
+              showAlert={showAlert}
+              heading="Try TextUtills- Word counter, Download text, Character counter, Remove extra spaces"
+              mode={mode}
+            />
+          ),
+        },
+        { path: "/about", element: <About mode={mode} /> },
+      ],
+    },
+  ]);
   return (
+    <>
+      {/* <Navbar title="TextUtils"  mode={mode} toggleMode={toggleMode} />
+<Alert alert = {alert}/> */}
+      <RouterProvider router={router} />
+      {/* <div className="container my-3">
+  <Routes>
 
-<>
-<Navbar title="TextUtils"  mode={mode} toggleMode={toggleMode} />
-{/* <Navbar/> */}
-<Alert alert = {alert}/>
-<div className="container my-3">
-<TextForm  showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-<About mode={mode} />
-</div>
-</>
+<Route path='/' element={<TextForm  showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}/>  
+<Route path='/about' element={<About mode={mode} />}/>
+  </Routes>
+
+</div> */}
+    </>
   );
-  }
+}
 
 export default App;
